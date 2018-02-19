bafcc32ef1d8cfa0524e46f0596be49788a84270
Jessexd <Jessexd@users.noreply.github.com>
2018-02-18 20:16:42 -0500

    fixed due to reviewers requested changes

--- plugins/speccy.py
+++ plugins/speccy.py
@@ -7,14 +7,20 @@ import requests
 from bs4 import BeautifulSoup as BS
 from contextlib import closing
 
-url_re = re.compile('https?:\/\/speccy.piriform.com\/results\/[A-z0-9]+', re.I)
+url_re = re.compile('https?:\/\/speccy\.piriform\.com\/results\/[a-zA-Z0-9]+',
+                    re.I)
+
+PICO_RE = re.compile('.*pico', re.I)
+KMS_RE = re.compile('.*kms', re.I)
+BOOSTER_RE = re.compile('.*booster', re.I)
+REVIVER_RE = re.compile('.*reviver', re.I)
+KILLER_RE = re.compile('.*Killer.+Service', re.I)
 
 
 @hook.regex(url_re)
-def get_speccy_url(message, match, chan, nick):
+def get_speccy_url(match):
 
-    with closing(requests.get(match.group(), stream=True,
-                              timeout=3)) as response:
+    with closing(requests.get(match.group(), )) as response:
         soup = BS(response.content, "lxml", from_encoding=response.encoding)
 
     body = soup.body
@@ -37,33 +43,34 @@ def get_speccy_url(message, match, chan, nick):
             "\x02CPU:\x02" + " " + cpuspec.next_sibling.next_sibling.text)
 
     gpufind = body.find("div", text='Graphics').next_sibling.next_sibling.text
+    GPU_RE = re.finditer(
+        r'.*(amd|radeon|intel|integrated|nvidia|geforce|gtx).*\n.*', gpufind,
+        re.IGNORECASE)
     gpuspec = ""
-    for gpustring in re.finditer(
-            r'.*(amd|radeon|intel|integrated|nvidia|geforce|gtx).*\n.*',
-            gpufind, re.IGNORECASE):
+    for gpustring in GPU_RE:
         gpuspec += gpustring.group()
     if gpuspec:
         data.append("\x02GPU:\x02" + " " + gpuspec)
 
-    picospec = body.find("div", text=re.compile('.*pico', re.I))
+    picospec = body.find("div", text=PICO_RE)
     if picospec:
-        data.append("\x02Badware:\x02" + " " + picospec.text)
+        data.append("\x02Bad:\x02" + " " + picospec.text)
 
-    kmsspec = body.find("div", text=re.compile('.*kms', re.I))
+    kmsspec = body.find("div", text=KMS_RE)
     if kmsspec:
-        data.append("\x02Badware:\x02" + " " + kmsspec.text)
+        data.append("\x02Bad:\x02" + " " + kmsspec.text)
 
-    boosterspec = body.find("div", text=re.compile('.*booster', re.I))
+    boosterspec = body.find("div", text=BOOSTER_RE)
     if boosterspec:
-        data.append("\x02Badware:\x02" + " " + boosterspec.text)
+        data.append("\x02Bad:\x02" + " " + boosterspec.text)
 
-    reviverspec = body.find("div", text=re.compile('.*reviver', re.I))
+    reviverspec = body.find("div", text=REVIVER_RE)
     if reviverspec:
-        data.append("\x02Badware:\x02" + " " + reviverspec.text)
+        data.append("\x02Bad:\x02" + " " + reviverspec.text)
 
-    killerspec = body.find("div", text=re.compile('.*Killer.+Service', re.I))
+    killerspec = body.find("div", text=KILLER_RE)
     if killerspec:
-        data.append("\x02Badware:\x02" + " " + killerspec.text)
+        data.append("\x02Bad:\x02" + " " + killerspec.text)
 
     def smartcheck():
         drivespec = body.find_all("div", text="05")
@@ -79,19 +86,11 @@ def get_speccy_url(message, match, chan, nick):
                 values.append(str(i + 1))
         return values
 
-    try:
-        z = smartcheck()
-        if len(z) != 0:
-            smartspec = ""
-            for item in z:
-                smartspec += data.append("\x02Bad Disk:\x02" +
-                                         (smartspec) + " #" + item + " ")
-        else:
-            smartspec = None
-    except Exception:
-        smartspec = None
+    z = smartcheck()
+    if z:
+        for item in z:
+            data.append("\x02Bad Disk:\x02 #{}".format(item))
 
-    specout = re.sub("\s{2,}|\r\n|\n", " ", str(' + '.join(data)))
+    specout = re.sub(r"\s+", " ", ' ● '.join(data))
 
     return specout
-
